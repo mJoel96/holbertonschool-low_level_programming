97c13a888f3516721ae53c3678d7f2802af9fede
mJoel96 <makharbinj@gmail.com>
2023-05-26 10:51:34 -0700

    m

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,19 +1,19 @@
 /*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
