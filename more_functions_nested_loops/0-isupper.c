71fd9a40b33fccdbaf9ea041ba79f65704b1c33b
mJoel96 <makharbinj@gmail.com>
2023-06-06 04:44:27 -0700

    m

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,12 @@
-#include "holberton.h"
-
-/**
- * _isupper - Check if the given character is an uppercase letter.
- * @c: Character to be tested
- *
- * Return: 1 if uppercase else 0 (False)
- */
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
