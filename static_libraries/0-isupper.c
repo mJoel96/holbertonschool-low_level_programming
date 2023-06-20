b93c2e0e0bac67dfced47cea7b5ed12454d2b17d
mJoel96 <makharbinj@gmail.com>
2023-06-19 20:11:29 -0700

    pop

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -2,10 +2,14 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * @c: the character to check
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if(c >= 'A' && c <= 'Z')
+		return(1);
+	else
+		return(0);
 }
